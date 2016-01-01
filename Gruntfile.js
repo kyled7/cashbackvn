86ee32661437bb51a3c3bb5d1f7c2b63028e0cd9
kyledinh <kyledinh.vn@gmail.com>
2016-01-01 16:48:22 +0700

    Grunt shipit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        shipit: {
+            options: {
+                ignores: ['node_modules', 'bower_components'],
+                workspace: '.',
+                keepReleases: 2,
+                servers: 'ec2-user@ec2-52-74-55-153.ap-southeast-1.compute.amazonaws.com'
+            },
+            development: {
+                deployTo: '/var/www/cashbackvn/dev'
+            },
+            staging: {
+                deployTo: '/var/www/cashbackvn/stage'
+            },
+            production: {
+                deployTo: '/var/www/cashbackvn/prod'
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-shipit');
+    grunt.registerTask('deploy', ['deploy:init', 'deploy:update', 'deploy:publish', 'deploy:clean']);
+};
