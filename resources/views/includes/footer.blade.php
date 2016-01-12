2bad897380e9eb8066977eeb925a54ef14e5257c
Kyle Dinh <kyledinh.vn@gmail.com>
2016-01-12 17:52:44 +0700

    Footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,38 @@
 <div class="footer">
-    © Copyright 2015 Kyle
+    <div class="container">
+        <div class="clearfix">
+            <div class="footer-logo">
+                <a href="/">HoanTien.VN</a>
+                <small>Phiên bản thử nghiệm</small>
+            </div>
+            <dl class="footer-nav">
+                <dt class="nav-title">THÔNG TIN HỖ TRỢ</dt>
+                <dd class="nav-item"><a href="#">Cách thức hoạt động</a></dd>
+                <dd class="nav-item"><a href="#">FAQ</a></dd>
+                <dd class="nav-item"><a href="#">Điểu khoản sử dụng</a></dd>
+                <dd class="nav-item"><a href="#">Chính sách bảo mật</a></dd>
+            </dl>
+            <dl class="footer-nav">
+                <dt class="nav-title">GIỚI THIỆU</dt>
+                <dd class="nav-item"><a href="#">Về công ty</a></dd>
+                <dd class="nav-item"><a href="#">Liên hệ</a></dd>
+            </dl>
+            <dl class="footer-nav last-footer-nav">
+                <dt class="nav-title">Kết nối HoanTien.VN</dt>
+                <dd class="nav-item">
+                    <a href="#">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                </dd>
+                <dt class="nav-title">Đăng ký nhận thông báo</dt>
+                <form>
+                    <input type="email" placeholder="Nhập email của bạn" name="email">
+                </form>
+            </dl>
+        </div>
+        <div class="footer-copyright text-center">Bản quyền © 2015 <a href="/">HoanTien.VN</a> </div>
+    </div>
 </div>
